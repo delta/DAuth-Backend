93451ef465e38fa7eb523e6516eed4bc70d66bb7
siva2204 <sivaraman26r@gmail.com>
2021-06-13 02:15:24 +0530

    [Add][cors] frontend

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import session from 'express-session';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
@@ -14,15 +14,27 @@ import { initialisePassport } from './config/passport';
 
 const port = process.env.PORT;
 
+// Body parsing Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Helmet Middleware
 app.use(helmet());
 
 // cors Middleware
-app.use(cors());
-
-// Body parsing Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'Authorization'
+    ]
+  })
+);
 
 // Session Middleware
 app.use(
