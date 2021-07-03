ecc1acaef4c8fe3ce82206aeaf5ffd3756d0520b
Sindhu <yellasindhu09@gmail.com>
2021-07-03 13:09:52 +0530

    fix: Change message

--- src/controllers/dashboardController.ts
+++ src/controllers/dashboardController.ts
@@ -4,50 +4,46 @@ import { Request, Response } from 'express';
 //returns userInfo for dashboard
 export const userInfo = async (req: Request, res: Response): Promise<unknown> => {
   const user: any = req.user
-  if (user && user.id) {
-    try{
-      const userInfo: any = await prisma.resourceOwner.findUnique({
-        where: {
-          id: user.id
+  try{
+    const userInfo: any = await prisma.resourceOwner.findUnique({
+      where: {
+        id: user.id
+      },
+      select: {
+        name: true,
+        phoneNumber: true,
+        department: true,
+        year: true,
+        email:{
+          select:{
+            email: true
+          }
         },
-        select: {
-          name: true,
-          phoneNumber: true,
-          department: true,
-          year: true,
-          email:{
-            select:{
-              email: true
-            }
-          },
-          authorizedApps:{
-            select:{
-              client:{
-                select:{
-                  id: true,
-                  name: true,
-                  redirectUri: true
-                }
-              },
-              createdAt: true
-            }
+        authorizedApps:{
+          select:{
+            client:{
+              select:{
+                id: true,
+                name: true,
+                redirectUri: true
+              }
+            },
+            createdAt: true
           }
         }
-      });
-      return res.status(200).json( userInfo );
-    }catch (error) {
-      console.log(error)
-      return res.status(500).json({ message: 'Internal server error' });
-    }
-    
+      }
+    });
+    return res.status(200).json( userInfo );
+  }catch (error) {
+    console.log(error)
+    return res.status(500).json({ message: 'Internal server error' });
   }
-  return res.status(401).json({ message: 'Invalid request' });
 };
 
 
 export const removeAccess = async (req: Request, res: Response): Promise<unknown> => {
   const user : any = req.user;
-  if(user && user.id && req.body.clientId){
+  if(req.body.clientId){
     try {
       await prisma.authorisedApps.delete({
         where: {
@@ -59,7 +55,7 @@ export const removeAccess = async (req: Request, res: Response): Promise<unknown
       });
       return res.status(200).json({ message: 'Removed authorization to ' });
     } catch (error) {
-      return res.status(500).json({ message: 'Unable to process your request' });
+      return res.status(500).json({ message: 'Internal server error' });
     }
   }
   return res.status(401).json({ message: 'User not authenticated' });
