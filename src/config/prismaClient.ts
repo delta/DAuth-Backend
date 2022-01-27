712aeccb71cf0644dab0a51316f60779aff83124
siva <63258524+siva2204@users.noreply.github.com>
2022-01-27 23:41:21 +0530

    [Fix] delete client bug (#30)
    * [Fix] delete client bug
    
    * rm prisma logging

--- src/config/prismaClient.ts
+++ src/config/prismaClient.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 // TODO: configure logging
 // https://www.prisma.io/docs/concepts/components/prisma-client/working-with-prismaclient/logging
 const prisma = new PrismaClient({
-  log: ['query', 'info', `warn`, `error`]
+  log: [`warn`, `error`]
 });
 
 export default prisma;
